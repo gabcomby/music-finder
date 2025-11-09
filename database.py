924bc224c354535f3d6a52d37b2d029781dd4523
Gabriel Comby <combygabriel@gmail.com>
2025-11-09 18:21:07 -0500

    Identification works

--- database.py
+++ database.py
@@ -37,3 +37,49 @@ def addMusicToDatabase(musicPath):
     
     pickle.dump(database, open("database.pickle", "wb"), pickle.HIGHEST_PROTOCOL)
     pickle.dump(songIndex, open("song_index.pickle", "wb"), pickle.HIGHEST_PROTOCOL)
+
+def findMusicInDatabase(musicPath):
+    database = pickle.load(open("database.pickle", "rb"))
+
+    # 1. Read WAV file and extract the audio data (mono) and sampling frequency
+    samplingFreq, musicData = readWavFile(musicPath)
+
+    # 2. Perform Fourier Transform to get the music map
+    music_map = performFourierTransform(musicData, samplingFreq)
+
+    # 3. Generate music hashes from the music map
+    # Returns a dictionary of hashes (hash -> (time, song_id))
+    music_hashes = generate_music_hashes(music_map, None)
+
+
+    matches_per_song = {}
+    for hash, (sample_time, _) in music_hashes.items():
+        if hash in database:
+            matching_occurences = database[hash]
+            for source_time, song_index in matching_occurences:
+                if song_index not in matches_per_song:
+                    matches_per_song[song_index] = []
+                matches_per_song[song_index].append((hash, sample_time, source_time))
+            
+
+    # %%
+    scores = {}
+    for song_index, matches in matches_per_song.items():
+        song_scores_by_offset = {}
+        for hash, sample_time, source_time in matches:
+            delta = source_time - sample_time
+            if delta not in song_scores_by_offset:
+                song_scores_by_offset[delta] = 0
+            song_scores_by_offset[delta] += 1
+
+        max = (0, 0)
+        for offset, score in song_scores_by_offset.items():
+            if score > max[1]:
+                max = (offset, score)
+        
+        scores[song_index] = max
+
+    # Sort the scores for the user
+    scores = list(sorted(scores.items(), key=lambda x: x[1][1], reverse=True)) 
+    
+    return scores
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from database import addMusicToDatabase
+from database import addMusicToDatabase, findMusicInDatabase
 import scipy as sp
+import pickle
 
 def main():
-    addMusicToDatabase("data/Daft Punk - Television Rules The Nation.wav")
+    songIndex = pickle.load(open("song_index.pickle", "rb"))
+    # addMusicToDatabase("data/Daft Punk - Television Rules The Nation.wav")
+    matches = findMusicInDatabase("./data/MY OLD WAYS - (MORGAN PAGE BOOTLEG MIX) - Morgan Page.wav")
+    for song_id, score in matches:
+        print(f"{songIndex[song_id]}: Score of {score[1]} at {score[0]}")
 
 if __name__ == "__main__":
     main()
