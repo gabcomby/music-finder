12e2d9baa297763048d7c913d99f4b82f0b6d6bc
Gabriel Comby <combygabriel@gmail.com>
2025-11-16 09:28:55 -0500

    Audio recording now works

--- client/src/components/custom/AudioUploader.tsx
+++ client/src/components/custom/AudioUploader.tsx
@@ -1,23 +1,49 @@
 import { Button } from "../ui/button";
 import { Mic } from "lucide-react";
+import { Square } from "lucide-react";
 import {
   Dropzone,
   DropzoneContent,
   DropzoneEmptyState,
 } from "@/components/ui/shadcn-io/dropzone";
 import { useState } from "react";
+import { useAudioRecorder } from "@fixhq/react-audio-voice-recorder";
 
 const AudioUploader = () => {
+  // For file uploader
   const [files, setFiles] = useState<File[] | undefined>();
   const handleDrop = (files: File[]) => {
     console.log(files);
     setFiles(files);
   };
+
+  // For live audio recorder
+  const {
+    startRecording,
+    stopRecording,
+    recordingBlob,
+    isRecording,
+    recordingTime,
+  } = useAudioRecorder();
+
   return (
     <div className="flex flex-col w-1/6 gap-4">
-      <Button variant="outline" size="sm">
-        <Mic /> Record Live Audio
-      </Button>
+      {isRecording ? (
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => {
+            stopRecording();
+            if (recordingBlob) console.log(recordingBlob);
+          }}
+        >
+          <Square /> Stop Recording ({recordingTime}s)
+        </Button>
+      ) : (
+        <Button variant="outline" size="sm" onClick={startRecording}>
+          <Mic /> Record Live Audio
+        </Button>
+      )}
       <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-center">
         or
       </h4>
